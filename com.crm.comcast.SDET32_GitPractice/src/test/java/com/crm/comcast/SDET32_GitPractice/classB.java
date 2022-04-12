6d47882db56b08eabab3ac25b21db40bb8c8b2bf
hp <hp@DESKTOP-60USC5F.mshome.net>
2022-04-12 12:36:02 +0530

    commit 3

--- com.crm.comcast.SDET32_GitPractice/src/test/java/com/crm/comcast/SDET32_GitPractice/classB.java
+++ com.crm.comcast.SDET32_GitPractice/src/test/java/com/crm/comcast/SDET32_GitPractice/classB.java
@@ -7,5 +7,9 @@ public class classB {
 	@Test
 	public void demoTest2() {
 		System.out.println("demoTest2");
+		System.out.println("demoTest1");
+		System.out.println("demoTest3");
+		System.out.println("demoTest4");
+
 	}
 }
