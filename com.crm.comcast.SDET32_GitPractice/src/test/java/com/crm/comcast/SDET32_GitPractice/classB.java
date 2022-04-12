bd9cca2ea302b8713acbcfab5ddaed84d4966f21
hp <hp@DESKTOP-60USC5F.mshome.net>
2022-04-12 12:20:40 +0530

    commit 2

---
+++ com.crm.comcast.SDET32_GitPractice/src/test/java/com/crm/comcast/SDET32_GitPractice/classB.java
@@ -0,0 +1,11 @@
+package com.crm.comcast.SDET32_GitPractice;
+
+import org.testng.annotations.Test;
+
+public class classB {
+
+	@Test
+	public void demoTest2() {
+		System.out.println("demoTest2");
+	}
+}
