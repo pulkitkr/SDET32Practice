aa6e96288b244bbe007a9847ccb1a529fac27233
hp <hp@DESKTOP-60USC5F.mshome.net>
2022-04-12 11:39:40 +0530

    commit 1

---
+++ com.crm.comcast.SDET32_GitPractice/src/test/java/com/crm/comcast/SDET32_GitPractice/ClassA.java
@@ -0,0 +1,11 @@
+package com.crm.comcast.SDET32_GitPractice;
+
+import org.testng.annotations.Test;
+
+public class ClassA {
+
+	@Test
+	public void demoTest() {
+		System.out.println("first push");
+	}
+}
