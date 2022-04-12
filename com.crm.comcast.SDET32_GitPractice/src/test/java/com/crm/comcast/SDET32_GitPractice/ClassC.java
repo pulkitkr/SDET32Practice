8723c3bf2f222ce6973db7e9e814563bbb4b661c
hp <hp@DESKTOP-60USC5F.mshome.net>
2022-04-12 12:44:36 +0530

    commit 4

--- com.crm.comcast.SDET32_GitPractice/src/test/java/com/crm/comcast/SDET32_GitPractice/ClassC.java
+++ com.crm.comcast.SDET32_GitPractice/src/test/java/com/crm/comcast/SDET32_GitPractice/ClassC.java
@@ -2,14 +2,16 @@ package com.crm.comcast.SDET32_GitPractice;
 
 import org.testng.annotations.Test;
 
-public class classB {
+public class ClassC {
 
 	@Test
-	public void demoTest2() {
+	public void demoTest3() {
 		System.out.println("demoTest1");
 		System.out.println("demoTest2");
 		System.out.println("demoTest3");
 		System.out.println("demoTest4");
+		System.out.println("commit 4");
+		
 
 	}
 }
